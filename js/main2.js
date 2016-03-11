0f41e39e5af32ff08e0c5c2b4877b487081b9e81
Joel Margate <sindicatoesp@gmail.com>
2016-03-11 17:17:16 +0100

    CDN for text.js

--- js/main2.js
+++ js/main2.js
@@ -1,21 +1,30 @@
 // Filename: main2.js
 
 // Require.js allows us to configure shortcut alias
-require.config({
+var margateConfig = require.config({
 	baseUrl: 'js',
 	paths: {
 		jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.1/jquery.min',
 		underscore: 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
 		backbone: 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min',
-		requiretext: 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min.js'
-	}
+		text: 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min'
+	},
+		shim: {
+			// Backbone
+			backbone: {
+				 // Depends on underscore/lodash and jQuery
+				 deps: ["underscore", "jquery"],
+				 // Exports the global window.Backbone object
+				 exports: "Backbone"
+			}
+	    }
 });
 
 
-require([
+margateConfig([
   // Load our app module and pass it to our definition function
-  'app2',
-], function(App){
+  'require', 'app2',
+], function(require, App){
   // The "app" dependency is passed in as "App"
   App.initialize();
 });
